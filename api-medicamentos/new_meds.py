c1535c2fcb358734f04c48e9853ba16a5ec12e96
Lenin Chavez <lenin.chavez@utec.edu.pe>
2023-11-26 15:49:58 -0500

    Finish scan_meds and new_meds lambda

--- api-medicamentos/new_meds.py
+++ api-medicamentos/new_meds.py
@@ -6,16 +6,15 @@ import os
 def lambda_handler(event, context):
     # Entrada (json)
     print(event)  # Revisar en CloudWatch
-    body = json.loads(event["Records"][0]["body"])
-    Message = json.loads(body["Message"])
+    # body = json.loads(event["Records"][0]["Sns"])
+    # print(body)  # Revisar en CloudWatch
+    fabricant_id = event["Records"][0]["Sns"]['MessageAttributes']['fabricant_id']['Value']
+    info = event["Records"][0]["Sns"]['MessageAttributes']['info']['Value']
     nombre_tabla = os.environ["TABLE_NAME"]
-    
+
     dynamodb = boto3.resource("dynamodb")
     table = dynamodb.Table(nombre_tabla)
-    archivo = {
-        "fabricant_id": Message["fabricant_id"],
-        "info": Message["info"],
-    }
+    archivo = {"fabricant_id": fabricant_id, "info": info}
     print(archivo)  # Revisar en CloudWatch
     response = table.put_item(Item=archivo)
     # Salida (json)
